7d04fd6919f6df923923c486ff79c2ef4003e0bf
shinystarlight00 <charles.dev.0811@gmail.com>
2024-05-31 17:39:13 +0800

    style changed

--- login.php
+++ login.php
@@ -23,21 +23,27 @@ color:white;
   include "config.php";
   if($_POST)
 	{
+
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 		
 		$sql = "SELECT * FROM `register` where email = '".$email."' and password = '".$password."' ";
 		$query =  mysqli_query($conn, $sql);
+
 		if(mysqli_num_rows($query)>0)
 		{
+
 			$row = mysqli_fetch_assoc($query);
 			session_start();
 			$_SESSION['name'] = $row['name'];
 			header('Location: home.php');
+
 		}
 		else
 		{
+
 			echo "<script> alert('Invalid Email or Password.'); </script>";
+      
 		}
 	}
 ?>
